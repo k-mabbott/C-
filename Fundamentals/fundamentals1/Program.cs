224163b74058789eda56be23792f54eb7a3a1cd3
k-mabbott <kmabbott9@gmail.com>
2023-07-02 20:03:03 -0700

    commit

---
+++ Fundamentals/fundamentals1/Program.cs
@@ -0,0 +1,170 @@
+﻿// See https://aka.ms/new-console-template for more information);
+
+Random rand = new Random();
+
+// Create a loop that prints all values from 1-255.
+
+for (int i = 1; i < 256; i++)
+{
+    Console.WriteLine($"the number is {i}");
+}
+
+
+// Create a new loop that generates 5 random numbers between 10 and 20.
+
+for (int i = 1; i < 6 ; i++)
+{
+    Console.WriteLine(rand.Next(10,21));
+}
+
+// Modify the previous loop to add the random values together and print the sum after the 
+// loop finishes.
+
+int sum = 0;
+for (int i = 1; i < 6 ; i++)
+{
+    sum = sum + rand.Next(10,21);
+}
+Console.WriteLine(sum);
+
+// Create a new loop that prints all values from 1 to 100 that are divisible by 3 OR 5, but 
+// NOT both.
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        continue;
+    }
+    else if (i % 3 == 0 || i % 5 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+// Modify the previous loop to print "Fizz" for multiples of 3 and "Buzz" for multiples of 5.
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        continue;
+    }
+    else if (i % 3 == 0 )
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+}
+
+// Modify the previous loop once more to now also print "FizzBuzz" for numbers that are 
+// multiples of both 3 and 5.
+
+for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0 )
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+}
+
+// (Optional) If you used for loops for your solutions, try doing the same with while loops. 
+// Vice versa if you used while loops!
+
+// int i = 1;
+
+// Create a loop that prints all values from 1-255.
+
+// while ( i < 256)
+// {
+//     Console.WriteLine($"the number is {i}");
+//     i++;
+// }
+
+
+// Create a new loop that generates 5 random numbers between 10 and 20.
+
+// while ( i < 6 )
+// {
+//     Console.WriteLine(rand.Next(10,21));
+//     i++;
+// }
+
+// Modify the previous loop to add the random values together and print the sum after the 
+// loop finishes.
+
+// int sum = 0;
+// while ( i < 6 )
+// {
+//     sum = sum + rand.Next(10,21);
+//     i++;
+// }
+// Console.WriteLine(sum);
+
+// Create a new loop that prints all values from 1 to 100 that are divisible by 3 OR 5, but 
+// NOT both.
+
+// while ( i < 101)
+// {
+//     if (i % 3 == 0 && i % 5 == 0)
+//     {
+//         i++;
+//         continue;
+//     }
+//     else if (i % 3 == 0 || i % 5 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     i++;
+// }
+
+// Modify the previous loop to print "Fizz" for multiples of 3 and "Buzz" for multiples of 5.
+
+// while (i < 101)
+// {
+//     if (i % 3 == 0 && i % 5 == 0)
+//     {
+//         i++;
+//         continue;
+//     }
+//     else if (i % 3 == 0 )
+//     {
+//         Console.WriteLine("Fizz");
+//     }
+//     else if (i % 5 == 0)
+//     {
+//         Console.WriteLine("Buzz");
+//     }
+//     i++;
+// }
+
+// Modify the previous loop once more to now also print "FizzBuzz" for numbers that are 
+// multiples of both 3 and 5.
+
+// while ( i < 101)
+// {
+//     if (i % 3 == 0 && i % 5 == 0)
+//     {
+//         Console.WriteLine("FizzBuzz");
+//     }
+//     else if (i % 3 == 0 )
+//     {
+//         Console.WriteLine("Fizz");
+//     }
+//     else if (i % 5 == 0)
+//     {
+//         Console.WriteLine("Buzz");
+//     }
+//     i++;
+// }
---
+++ Fundamentals/fundamentals2/Program.cs
@@ -0,0 +1,77 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+// Directions
+
+// Complete the following exercises:
+// Three Basic Arrays
+
+//     Create an integer array with the values 0 through 9 inside.
+//     Create a string array with the names "Tim", "Martin", "Nikki", and "Sara".
+//     Create a boolean array of length 10. Then fill it with alternating true/false 
+// values, starting with true. (Tip: do this using logic! Do not hard-code the values in!)
+
+int[] numArr = new int[10] {0,1,2,3,4,5,6,7,8,9}; 
+string[] stringArr = new string[4] {"Tim", "Martin", "Nikki", "Sara"};
+bool[] boolArr = new bool [10];
+
+for (int i = 0; i < 10; i++)
+{
+    if (i % 2 == 0) 
+    {
+        boolArr[i] = true;
+    } else 
+    {
+        boolArr[i] = false;
+    }
+}
+
+// foreach (var item in boolArr)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+// List of Flavors
+
+//     Create a string List of ice cream flavors that holds at least 5 different flavors. 
+// (Feel free to add more than 5!)
+//     Output the length of the List after you added the flavors.
+//     Output the third flavor in the List.
+//     Now remove the third flavor using its index location.
+//     Output the length of the List again. It should now be one fewer.
+
+List<string> iceCream = new List<string>() {"Tonight Dough", "Mint Chocolate Chip"};
+iceCream.Add("Cookies & Cream");
+iceCream.Add("Vanilla");
+iceCream.Add("Rocky Road");
+Console.WriteLine(iceCream.Count);
+Console.WriteLine(iceCream[2]);
+iceCream.RemoveAt(2);
+Console.WriteLine(iceCream.Count);
+
+// User Dictionary
+
+//     Create a dictionary that will store string keys and string values.
+//     Add key/value pairs to the dictionary where:
+//         Each key is a name from your names array (this can be done by hand or using logic)
+//         Each value is a randomly selected flavor from your flavors List (remember Random from 
+// earlier?)
+//     Loop through the dictionary and print out each user's name and their associated ice cream 
+// flavor.
+
+Dictionary<string, string> user = new Dictionary<string, string>();
+
+Random rand = new Random();
+
+foreach (string name in stringArr)
+{
+    int idx = rand.Next(4);
+    user.Add(name, iceCream[idx]);
+}
+
+foreach (KeyValuePair<string, string> entry in user)
+{
+    Console.WriteLine($"{entry.Key} - {entry.Value}");
+}
