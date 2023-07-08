bdb2c0184e007d7641051bbe80a76de6872664a8
k-mabbott <kmabbott9@gmail.com>
2023-07-08 15:43:16 -0700

    Commit

--- ASPDotNet-core/SessionWorkshop/Controllers/HomeController.cs
+++ ASPDotNet-core/SessionWorkshop/Controllers/HomeController.cs
@@ -13,15 +13,70 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+// ------------------------------LANDING PAGE / INDEX
+
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+// ------------------------------LOGIN
+
+    [HttpPost("success")]
+    public IActionResult Success(string SesName)
     {
-        return View();
+        HttpContext.Session.SetString("UserName", SesName);
+        HttpContext.Session.SetInt32("UserNum", 5);
+        return RedirectToAction("Dashboard");
     }
+
+// ------------------------------LOGOUT
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+// ------------------------------DASHBOARD / SUCCESS
+
+    [HttpGet("dashboard")]
+    public IActionResult Dashboard()
+    {
+        if(HttpContext.Session.GetString("UserName") == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View("Dashboard");
+    }
+
+// ------------------------------MATH ROUTES
+
+    [HttpPost("math")]
+    public IActionResult Math(string amount)
+    {
+        int? TheNum = HttpContext.Session.GetInt32("UserNum");
+        if(amount == "add")
+        {
+            TheNum += 1;
+        } else if(amount == "sub") {
+            TheNum -= 1;
+        } else if(amount == "mult") {
+            TheNum = TheNum + TheNum;
+        } else if(amount == "rand") {
+            Random rand = new Random();
+            TheNum += rand.Next(1,11);
+        }
+        HttpContext.Session.SetInt32("UserNum", (int)TheNum);
+        return RedirectToAction("Dashboard");
+    }
+
+
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
